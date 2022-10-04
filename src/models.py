d6e26254d52e202a8424865c036b087c157d7406
Federico Galatolo <galatolo.federico@gmail.com>
2022-10-04 13:45:30 +0200

    ported model to pt lightning

--- src/models.py
+++ src/models.py
@@ -1,7 +1,9 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 from torchsummary import summary
 import torchvision.models as models
+from pytorch_lightning import LightningModule
 
 class EmbeddingNet(nn.Module):
 
@@ -54,11 +56,12 @@ class EmbeddingNet(nn.Module):
 
 
 
-class TripletNet(nn.Module):
-
-    def __init__(self, embedding_net):
+class TripletNet(LightningModule):
+    def __init__(self, embedding_net, lr=0.001):
         super(TripletNet, self).__init__()
         self.embedding_net = embedding_net
+        self.loss = nn.TripletMarginLoss(margin=1.0, p=2)
+        self.lr = lr
 
     def forward(self, x1, x2, x3):
         output1 = self.embedding_net(x1)
@@ -66,18 +69,32 @@ class TripletNet(nn.Module):
         output3 = self.embedding_net(x3)
         return output1, output2, output3
 
+    def training_step(self, batch, batch_nb):
+        (x1, x2, x3), _ = batch
+        h1, h2, h3 = self(x1, x2, x3)
+
+        loss = self.loss(h1, h2, h3)
+        self.log("train/loss", loss.item())
+
+        return loss
+
     def get_outputsize(self):
         return self.embedding_net.get_outputsize()
 
     def get_embedding(self, x):
         return self.embedding_net(x)
 
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
 
 
 if __name__ == '__main__':
     net = EmbeddingNet({'path': 'models', 'dropout': 0.15, 'output_size': 2, 'fc1_size': 64})
-
     triple_net = TripletNet(net)
 
+    '''
     summary(net, (3, 300, 300))
-    summary(triple_net, [(3, 300, 300), (3, 300, 300), (3, 300, 300)])
+    summary(triple_net, [(3, 300, 300), (3, 300, 300), (3, 300, 300)]) 
+    '''
+    img = torch.randn(2, 3, 300, 300)
+    triple_net(img, img, img)
